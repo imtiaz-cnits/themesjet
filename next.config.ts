f6c473f49f69c39a9a8639d0e28b16f1f127399d
imtiaz-cnits <ia64744@gmail.com>
2026-01-07 01:43:59 +0600

    simple changes 4

--- next.config.ts
+++ next.config.ts
@@ -54,7 +54,7 @@ const nextConfig: NextConfig = {
 
                 // Next.js will fetch the content from your cPanel server
                 // REPLACE 'https://YOUR_CPANEL_IP_OR_DOMAIN' with your actual cPanel URL
-                destination: 'https://160.191.80.34/templates/:path*',
+                destination: 'https://alpha.raw-server.com:2083/templates/:path*',
             },
         ];
     },
